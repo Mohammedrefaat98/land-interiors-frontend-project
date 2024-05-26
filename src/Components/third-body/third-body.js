b95c26438de66490febadae3886c5a16082254a4
mohammedrefaat98 <mohrefaat98@gmail.com>
2024-05-26 19:59:34 +0300

    add third body

--- src/Components/third-body/third-body.js
+++ src/Components/third-body/third-body.js
@@ -2,25 +2,53 @@
 import React from 'react'
 import './third-body.css'
 import kitchen from '../../assets/images/third-body-image4.png';
-import bedroom from '../../assets/images/third-body-image2.png';
-import livingRoom from '../../assets/images/third-body-image3.png';
-import homeOffice from '../../assets/images/third-body-image4.png';
+import bedroom from '../../assets/images/third-body-image3.png';
+import livingRoom from '../../assets/images/third-body-image2.png';
+import homeOffice from '../../assets/images/third-body-image1.png';
 
 
 export default function ThirdBody() {
   return (
     <div className='third-body'>
-        <p>What we do?</p>
-        <div className='row-container'>
-            <div className='card'>
+        <p className='title'>What we do?</p>
+        <div className='image-row-container'>
+            <div className='image-card'>
                 <div className='card-image-container'>
                     <img src={kitchen} alt='kitchen'/>
                 </div>
-                <div className='text-container'>
-                    <p>50 days or we pay you rent</p>
+                <div className='image-text-container'>
+                    <p>Modular Kitchen</p>
+                </div>
+            </div>
+            <div className='image-card'>
+                <div className='card-image-container'>
+                    <img src={bedroom} alt='kitchen'/>
+                </div>
+                <div className='image-text-container'>
+                    <p>Bedroom</p>
+                </div>
+            </div>
+            <div className='image-card'>
+                <div className='card-image-container'>
+                    <img src={livingRoom} alt='kitchen'/>
+                </div>
+                <div className='image-text-container'>
+                    <p>Living room</p>
+                </div>
+            </div>
+            <div className='image-card'>
+                <div className='card-image-container'>
+                    <img src={homeOffice} alt='kitchen'/>
+                </div>
+                <div className='image-text-container'>
+                    <p>Home office</p>
                 </div>
             </div>
         </div>
+        <div className='lower-text'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla</p>
+        </div>
+
     </div>
   )
 }
